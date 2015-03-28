d92937e3c6d6b09ac3b358b524ed5b62fa8ed3f6
Julian Wolf <julian.wolf@mail.mcgill.ca>
2015-03-28 19:14:20 -0400

    html_head subroutine; check quality of user inputs

--- Registration.pl
+++ Registration.pl
@@ -8,6 +8,7 @@ my $name = param('name');
 my $uname = param('uname');
 my $pass = param('pass');
 
+
 my $members_file = "members.csv";
 open(my $fh,'<',$members_file);
 my @existing_users = <$fh>;
@@ -23,8 +24,7 @@ foreach (@existing_users) {
 	}
 }
 
-print "Content-Type:text/html\n\n";
-if ($user_exists) {
+sub html_head {
 	print "<html>\n";
 	print "<title> Become a Member </title>\n";
 	print "<center>\n";
@@ -32,6 +32,11 @@ if ($user_exists) {
 	print "		<b> PaperPlanes </b> <br />\n";
 	print "		Come fly with us. <br />\n";
 	print "	</head>\n";
+}
+
+print "Content-Type:text/html\n\n";
+if ($user_exists) {
+	&html_head;
 	print "	<body>\n";
 	print "		The username $uname is already taken; sorry about that. <br />\n";
 	print "		<a href=\"http://cs.mcgill.ca/~jwolf/register.html\">\n";
@@ -42,22 +47,25 @@ if ($user_exists) {
 	print "</html>\n";
 }
 else {
-	open(my $fh,'>>',$members_file);
-	print $fh "$name $uname $pass \n";
-	close($fh);
-	
-	print "<html>\n";
-	print "<title> Become a Member </title>\n";
-	print "<center>\n";
-	print "	<head>\n";
-	print "		<b> PaperPlanes </b> <br />\n";
-	print "		Come fly with us. <br />\n";
-	print "	</head>\n";
+	&html_head;
 	print "	<body>\n";
-	print "		Congratulations! Your account has been created. <br />\n";
-	print "		<a href=\"http://cs.mcgill.ca/~jwolf/welcome.html\">\n";
-	print "			Login now and get started!\n";
-	print "		</a>\n";
+	if ($name =~ /\W/ ||
+		$uname =~ /\W/ ||
+		$pass =~ /\W/) {
+		print " 	Only alphanumeric characters and underscores are allowed. <br />\n";
+		print "		<a href=\"http://cs.mcgill.ca/~jwolf/register.html\">\n";
+		print "			Try again?\n";
+		print "		</a>\n";
+	}
+	else {
+		open(my $fh,'>>',$members_file);
+		print $fh "$name $uname $pass \n";
+		close($fh);
+		print "		Congratulations! Your account has been created. <br />\n";
+		print "		<a href=\"http://cs.mcgill.ca/~jwolf/welcome.html\">\n";
+		print "			Login now and get started!\n";
+		print "		</a>\n";
+	}
 	print "	</body>\n";
 	print "</center>\n";
 	print "</html>\n";
